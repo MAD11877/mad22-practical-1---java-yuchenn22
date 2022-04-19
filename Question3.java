11441801fd30e6dd50d3619eb4abcc429b823519
Your own username <Your own email>
2022-04-19 16:05:54 +0800

    q2 and q3

--- Question3.java
+++ Question3.java
@@ -11,6 +11,9 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    
+    System.out.print("Enter Integer:");
+    int num = in.nextInt();
+    int result =(num*num);
+    System.out.println(result);
   }
 }
