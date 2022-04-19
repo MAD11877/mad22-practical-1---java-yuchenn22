f033b6ba2717f3658e8f376ccc69bfd33109556c
Your own username <Your own email>
2022-04-19 14:58:31 +0800

    q1 final

--- Question1.java
+++ Question1.java
@@ -1,5 +1,5 @@
-class Question1 {  
-  public static void main(String args[]) { 
+public class Question1 {  
+  public static void main(String[] args) { 
     System.out.println("Hello, world!"); 
   } 
 }
