b7e749754e35fd1eb216a28d90df75b4b70eb3e3
Your own username <Your own email>
2022-04-19 21:55:54 +0800

    q5

--- Question5.java
+++ Question5.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
-
+import java.util.*;
 public class Question5
 {
   public static void main(String[] args)
   {
+    List<Integer> numberList = new ArrayList<Integer>();
     /**
      * Prompt the user for number. This input indicates the number of integers the user will be entering next. 
      * Print out the mode (highest occurrence) from the set of integers. 
@@ -25,8 +26,29 @@ public class Question5
      *     2
      * Hint: Use a loop to get input. Use another 2 loops to find the mode
      */
-     
+    int maxCount = 0;
+    int ans = 0;
     Scanner in = new Scanner(System.in);
-    
+    System.out.print("Enter Integer: ");
+    int num = in.nextInt();
+    for (int i = 0; i<num; i++){
+      System.out.print("Enter number: ");
+      int number = in.nextInt();
+      numberList.add(number);
+    }
+    for (int u = 0; u<numberList.size(); u++){
+      int count = 0;
+      int store = (numberList.get(u));
+      for (int j = 0; j<numberList.size(); j++){
+        if (store ==  (numberList.get(j))){
+            count ++;
+        if (count > maxCount) {
+            maxCount = count;
+            ans = store;
+        }
+      }
+    }
   }
+  System.out.println(ans);
+}
 }
